0d1cb665b7c557014cde43e41692d7ce54239675
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-04-16 11:26:46 -0700

    fixed importing error

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from vllm_api import vLLMClient
-from analyze_evals import Evaluator
-from exceptions import ParseException, IllegalMoveException
+from .vllm_api import vLLMClient
+from .analyze_evals import Evaluator
+from .exceptions import ParseException, IllegalMoveException
