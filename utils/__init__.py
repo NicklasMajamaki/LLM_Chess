6a706f2f1ad75a1df871355da9567ef41d772be7
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-04-16 11:46:38 -0700

    more bugs

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .vllm_api import vLLMClient
 from .analyze_evals import Evaluator
+from .parsing import extract_solution, coerce_response
 from .exceptions import ParseException, IllegalMoveException
