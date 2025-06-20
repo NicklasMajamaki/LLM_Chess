75d20567d787f0bfcebd121cd0c273c3f4d624e5
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-06-20 12:47:53 -0700

    fix

--- llm_chess/utils/llm_parser.py
+++ llm_chess/utils/llm_parser.py
@@ -15,6 +15,7 @@ class LLMParser:
         self.args          = args
         self.runtype       = args.run_type
         self.wandb_run     = wandb_run
+        self.max_retry     = 1
         self.sys_prompt    = runtype_mapping[self.runtype]
         self.dataclasses   = [
             JSONFolderDataClass(args.data_dir, f, args.model_version, self.sys_prompt)
@@ -108,7 +109,7 @@ class LLMParser:
                 # fire-and-forget parsing tasks
                 await asyncio.gather(
                     *(
-                        _stream_parse(d, raw_responses[i], max_retry=self.args.max_retry)
+                        _stream_parse(d, raw_responses[i], max_retry=self.max_retry)
                         for i, d in enumerate(chunk)
                     )
                 )
